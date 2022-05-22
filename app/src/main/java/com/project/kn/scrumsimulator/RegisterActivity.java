2b939b8aa85480539f405b568a8e71d9b6f709e1
AndyDen98 <andenozr9@gmail.com>
2022-05-22 17:38:48 +0500

    add register form

---
+++ app/src/main/java/com/project/kn/scrumsimulator/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.project.kn.scrumsimulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.kn.scrumsimulator.config.DatabaseConfig;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_form);
+    }
+}
