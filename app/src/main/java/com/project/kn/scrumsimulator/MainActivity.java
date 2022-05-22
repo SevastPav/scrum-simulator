37faeee081be40e4b02812a68761d7d8cf85eff1
SevastPav <sevastyanov>
2022-05-22 18:36:44 +0500

    add dragable boardview

--- app/src/main/java/com/project/kn/scrumsimulator/MainActivity.java
+++ app/src/main/java/com/project/kn/scrumsimulator/MainActivity.java
@@ -3,15 +3,56 @@ package com.project.kn.scrumsimulator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.project.kn.scrumsimulator.boardview.BoardView;
+import com.project.kn.scrumsimulator.boardview.Item;
+import com.project.kn.scrumsimulator.boardview.SimpleBoardAdapter;
 import com.project.kn.scrumsimulator.config.DatabaseConfig;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main_old);
+//        DatabaseConfig db = new DatabaseConfig();
+//    }
+
+    ArrayList<Item> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DatabaseConfig db = new DatabaseConfig();
+        final BoardView boardView = (BoardView)findViewById(R.id.boardView);
+        boardView.SetColumnSnap(false);
+        boardView.SetColumnSnap(true);
+        final ArrayList<SimpleBoardAdapter.SimpleColumn> data = new ArrayList<>();
+        list.add(new Item("I am a very long list that is not the same size as the others. I am a multiline"));
+        list.add(new Item("Item 1"));
+        final ArrayList<Item> empty = new ArrayList<>();
+        final ArrayList<Item> test = new ArrayList<>();
+        test.add(new Item("Item 1"));
+        test.add(new Item("Item 1"));
+        test.add(new Item("Item 1"));
+        test.add(new Item("Item 1"));
+        data.add(new SimpleBoardAdapter.SimpleColumn("Column 1",(ArrayList)list));
+        data.add(new SimpleBoardAdapter.SimpleColumn("Column 2",(ArrayList)test));
+        data.add(new SimpleBoardAdapter.SimpleColumn("Column 3",(ArrayList)empty));
+        data.add(new SimpleBoardAdapter.SimpleColumn("Column 4",(ArrayList)empty));
+        data.add(new SimpleBoardAdapter.SimpleColumn("Column 5",(ArrayList)empty));
+        data.add(new SimpleBoardAdapter.SimpleColumn("Column 6",(ArrayList)empty));
+        data.add(new SimpleBoardAdapter.SimpleColumn("Column 7",(ArrayList)empty));
+        final SimpleBoardAdapter boardAdapter = new SimpleBoardAdapter(this,data);
+        boardView.setAdapter(boardAdapter);
+        boardView.setOnDoneListener(new BoardView.DoneListener() {
+            @Override
+            public void onDone() {
+                Log.e("scroll","done");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/project/kn/scrumsimulator/boardview/Item.java
@@ -0,0 +1,14 @@
+package com.project.kn.scrumsimulator.boardview;
+
+public class Item{
+
+    public String item;
+
+    public Item(String item){
+        this.item = item;
+    }
+
+    public String toString(){
+        return item;
+    }
+}
