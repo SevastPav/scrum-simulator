a1c04d1fe39da195a00dea95b784dfebed1ee74e
AndyDen98 <andenozr9@gmail.com>
2022-05-29 13:32:51 +0500

    SCR-2 базовая настройка игры

---
+++ app/src/main/java/com/project/kn/scrumsimulator/StartPage.java
@@ -0,0 +1,29 @@
+package com.project.kn.scrumsimulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartPage extends AppCompatActivity {
+
+    public static Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+
+        startButton = findViewById(R.id.start_game);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
